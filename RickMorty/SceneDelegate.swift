11cc1b7bf11589aa8991ff36b2507c7e6e789c53
Michael Kolkov <55915846+mireabot@users.noreply.github.com>
2023-08-04 11:12:12 -0400

    Initial commit

---
+++ RickMorty/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  RickMorty
+//
+//  Created by Mikhail Kolkov on 8/4/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+
+
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    let vc = ViewController()
+    
+    let window = UIWindow(windowScene: windowScene)
+    window.rootViewController = vc
+    window.makeKeyAndVisible()
+    
+    self.window = window
+  }
+}
+
---
+++ RickMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickMorty
+//
+//  Created by Mikhail Kolkov on 8/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .red
+  }
+
+
+}
+
